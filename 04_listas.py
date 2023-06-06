5238b151d34461fac30f5880c4dc7444f003aa8c
Ckott <diegoangeles130703@gmail.com>
2023-06-05 21:18:58 -0500

    listaspy

--- 04_listas.py
+++ 04_listas.py
@@ -10,7 +10,7 @@ my_list = [35,24,62,52,30,30,17]
 print(my_list)
 print(len(my_list))
 
-my_other_list = [35,1.77,"Scott","Angeles"]
+my_other_list = [19,1.77,"Scott","Angeles"]
 
 print(type(my_list))
 print(type(my_other_list))
@@ -22,4 +22,10 @@ print(my_other_list[-3])
 print(my_other_list[-4])
 # print(my_other_list[4]) #IndexError
 # print(my_other_list[-5]) #IndexError
+
+age,height,name,surname = my_other_list
+
+print(name)
+
+
 
