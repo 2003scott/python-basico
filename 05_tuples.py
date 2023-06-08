2c09e63615ebce6663af9c378018fea0137d0898
Ckott <diegoangeles130703@gmail.com>
2023-06-08 18:48:11 -0500

    tuples

--- 05_tuples.py
+++ 05_tuples.py
@@ -3,7 +3,7 @@
 my_tuple = tuple()
 my_other_tuple = ()
 
-my_tuple = (19,1.74,"Scott","Angeles")
+my_tuple = (19,1.74,"Scott","Angeles","Scott")
 my_other_tuple = (35,60,30)
 
 print(my_tuple)
@@ -17,6 +17,25 @@ print(my_tuple[-1])
 print(my_tuple.count("Scott")) #Cuenta cuantos datos hay iguales dentro de la tupla
 print(my_tuple.index("Angeles")) # Nos dicen en que posicion esta
 
-# my_tuple[] = 1.80
-# print(my_tuple)
+# my_tuple[] = 1.80 'tuple' object does not suppoty iteam assignment
+
+my_sum_tuple = my_tuple + my_other_tuple
+print(my_sum_tuple[3:6])
+
+my_tuple = list(my_tuple)
+print(my_tuple)
+
+my_tuple[4] = "Dscott"
+my_tuple.insert(1,"Azul") # Inserta un dato ala tupla
+my_tuple= tuple(my_tuple)
+print(my_tuple)
+print(tuple(my_tuple))
+
+del my_tuple[2]
+print(my_tuple)
+
+del my_tuple
+# print(my_tuple) NameError : __name__ "My_tyuple" is not defined
+
+
 
